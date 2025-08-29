b13c097ad4938d08e602bc5de85061461a62b61a
[Adrin Neville] <[adrinneville11@gmail.com]>
2025-08-29 17:10:13 +0530

    [authentication]

--- models.py
+++ models.py
@@ -1,17 +1,9 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Enum as SQLEnum
-from enum import Enum
-
-class RoleEnum(str, Enum):
-    ADMIN = "admin"
-    USER = "user"
-    MODERATOR = "moderator"
-    GUEST = "guest"
+from sqlalchemy import Column, Integer, String
 
 class Users(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)  # <-- fix here
     hashed_password = Column(String)
-    role = Column(SQLEnum(RoleEnum), default=RoleEnum.USER)
